b41ce71ef3005db03ecba3ce6c9597fedb6aec21
Juicpt <wdxpn@hotmail.com>
2020-01-09 13:34:04 +0800

    welcome 样式修改

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,35 +1,52 @@
 <template>
-    <div>
-        <h3>Welcome</h3>
-        <el-row :gutter="20">
-  <el-col :span="10"><div class="grid-content_bg-purple">
- <el-date-picker v-model="selectMonth" type="month" placeholder="选择月"   @change="jobSearch" value-format="yyyy-MM"></el-date-picker>
-    </div>
-    </el-col>
-</el-row>
-<el-row :gutter="20">
-  <el-col :span="14"><div class="grid-content_bg-purple">
-    <el-card class="box-card-1">
-  <div slot="header" class="clearfix">
-    <span></span>
-    <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
+  <div>
+    <h3>Welcome</h3>
+    <el-row :gutter="20">
+      <el-col :span="10">
+        <div class="grid-content_bg-purple">
+          <el-date-picker type="month" placeholder="选择月"
+                          value-format="yyyy-MM"/>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="14">
+        <div class="grid-content_bg-purple">
+          <el-card class="box-card-1">
+            <div slot="header" class="clearfix">
+              <span/>
+              <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
+            </div>
+          </el-card>
+        </div>
+      </el-col>
+    </el-row>
   </div>
-</el-card>
-    </div>
-    </el-col>
-</el-row>
-    </div>
 
 </template>
 <script>
-export default {}
+  export default {}
 </script>
 
 <style lang="less" scoped>
-.grid-content_bg-purple {
+  .grid-content_bg-purple {
     margin-bottom: 40px;
     margin-left: 60px;
     background-color: #C6E2FF;
 
-}
+  }
+
+  .box-card-1 {
+    background-color: rgba(1, 1, 1, 0);
+    border: unset;
+
+    /deep/ .el-card__header {
+      border-bottom: unset !important;
+    }
+
+    /deep/ .el-card__body {
+      border-bottom: unset !important;
+    }
+  }
+
 </style>
